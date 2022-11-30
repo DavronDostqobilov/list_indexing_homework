2c7d54b3874c89c748b9d92e5c94e19998390042
DavronDostqobilov <dostqobilovdavron885@gmail.com>
2022-12-01 02:06:31 +0500

    List10

--- List10.py
+++ List10.py
@@ -6,4 +6,8 @@ def main(list_num):
     Returns:
         int: return answer
     """
-    return
+    if list_num[0]<list_num[-1]:
+        return list_num[-1]
+    else:
+        return list_num[0]
+print(main([23,34,54,65,100]))
